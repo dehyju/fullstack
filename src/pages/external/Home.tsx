a228684f65146fa6333eae0459cf162656d6c4dc
dehyju <stephe.t.j.leong@gmail.com>
2025-06-25 17:23:31 +0100

    Making the pages look nicer

--- src/pages/external/Home.tsx
+++ src/pages/external/Home.tsx
@@ -2,7 +2,10 @@ import logo from "../../assets/logo.svg"
 function Home() {
   return (
     <div className="flex flex-col items-center">
-      <img className="flex rounded-3xl" src={logo}/>
+      <div className="flex justify-center items-center text-5xl gap-x-4">
+        <img className="flex h-[2em] rounded-2xl" src={logo}/>
+        <h1 className="text-[1.1em]"><span className="font-bold">Stephen</span>Leong</h1>   
+      </div>
     </div>
   )
 }
